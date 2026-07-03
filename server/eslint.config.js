999efcf3b58f3523cd3a7e58cb87c4922d6bd19c
ryhndastra <ryhndastra@gmail.com>
2026-07-03 12:11:37 +0700

    feat(server): initialize express backend architecture

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -7,4 +7,16 @@ export default [
   {
     ignores: ["dist", "node_modules"],
   },
+  {
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
 ];
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import compression from "compression";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
-
+import { errorMiddleware } from "./middlewares/error.middleware.js";
+import { notFoundMiddleware } from "./middlewares/not-found.middleware.js";
 import healthRoute from "./routes/health.route.js";
 
 const app = express();
@@ -18,8 +20,11 @@ app.use(
 app.use(helmet());
 app.use(compression());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-
+app.use(cookieParser());
+app.use(errorMiddleware);
+app.use(notFoundMiddleware);
 app.use("/api/v1/health", healthRoute);
 
 export default app;
